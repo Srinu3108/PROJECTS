d4babb4a2385b7d73722f6576c9c1510d3c693ef
Srinu3108 <124229970+Srinu3108@users.noreply.github.com>
2026-07-19 22:42:44 +0530

    Delete EnergyPulse folder

--- EnergyPulse/EnergyPulse/DTOs/CreateDeviceDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EnergyPulse.Api.DTOs
-{
-    public class CreateDeviceDto
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Status { get; set; } = string.Empty;
-        public int SiteId { get; set; }
-    }
-}
